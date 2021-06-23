2ccde9d4028de2e3dde2b8a4c70625009bbf59d5
박찬하 <chanhacraft@naver.com>
2021-06-23 12:38:23 +0900

    first commit

---
+++ src/test/java/com/chcraft/dogblacklist/WartestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chcraft.dogblacklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WartestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
